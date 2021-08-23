827ee51cb313cc4806febc0fb125a38e90367a4e
崔鹏举 <pengju.cui@eoitek.com>
2021-08-23 17:59:03 +0800

    添加login页路由

--- README.md
+++ README.md
@@ -22,3 +22,29 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## 文档
+1.  ts 定义数据类型的语法
+  a.  ref: 定义相应的基础数据类型
+    获取值是通过 .value 获取。
+
+    const count = ref<number>(10);
+    console.log(count.value);
+
+  b.  reactive: 定义响应的对象类型
+
+    interface FormState {
+      user: string;
+      password: string;
+    };
+    const formState = reactive<FormState>({
+      user: '',
+      password: '',
+    });
+
+  c.  toRefs: 将响应式的对象转化为普通对象
+
+    import { toRefs } from 'vue';
+      b.内定义的对象...
+    const data = toRefs(formState);
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,15 +1,70 @@
 <template>
-  <div>
-    <a-button>按钮</a-button>
+  <div class="login flex-center">
+    <div class="form-wrap">
+      <a-form
+        class="flex-column"
+        layout="inline"
+        :model="formState"
+        @finish="handleFinish"
+        @finishFailed="handleFinishFailed"
+      >
+        <a-form-item class="pb-20" label="账号">
+          <a-input v-model:value="formState.user" placeholder="Username">
+            <template #prefix><UserOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+          </a-input>
+        </a-form-item>
+        <a-form-item class="pb-20" label="密码">
+          <a-input v-model:value="formState.password" type="password" placeholder="Password">
+            <template #prefix><LockOutlined style="color: rgba(0, 0, 0, 0.25)" /></template>
+          </a-input>
+        </a-form-item>
+        <a-form-item :wrapper-col="{ offset: 4 }">
+          <a-button
+            type="primary"
+            html-type="submit"
+            :disabled="formState.user === '' || formState.password === ''"
+          >
+            登录
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-
+<script lang="ts">
+import { UserOutlined, LockOutlined } from '@ant-design/icons-vue';
+import { ValidateErrorEntity } from 'ant-design-vue/es/form/interface';
+import { message } from 'ant-design-vue';
+import { defineComponent, reactive } from 'vue';
+interface FormState {
+  user: string;
+  password: string;
 }
+export default defineComponent({
+  name: 'login',
+  setup() {
+    const formState = reactive<FormState>({
+      user: '',
+      password: '',
+    });
+    const handleFinish = (values: FormState) => {
+      console.log(values, formState);
+      message.success('登录成功！');
+    };
+    const handleFinishFailed = (errors: ValidateErrorEntity<FormState>) => {
+      console.log(errors);
+    };
+    return {
+      formState,
+      handleFinish,
+      handleFinishFailed,
+    };
+  },
+  components: {
+    UserOutlined,
+    LockOutlined,
+  },
+})
 </script>
-
-<style>
-
-</style>
+<style lang="scss" src="./login.scss" scoped />
